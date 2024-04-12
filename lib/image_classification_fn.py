71bf049436d33d8fe90a2b6009de0b5cd6763ebd
Ashwin <ashes192000@gmail.com>
2024-04-12 12:01:29 +0530

    Refactor digit classification

---
+++ lib/image_classification_fn.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageOps
+import numpy as np
+
+def convert_rgba_to_rgb(image):
+    if image.mode == 'RGBA':
+        image = image.convert("RGBA")
+        canvas = Image.new('RGB', image.size, (255, 255, 255))
+        canvas.paste(image, mask=image.split()[3]) 
+        image = canvas
+    return image
+
+def convert_to_grayscale(image):
+    if image.mode in ['LA', 'P']:
+        image = ImageOps.grayscale(image.convert('RGBA'))
+    return image
+
+def resize_image(image):
+    aspect_ratio = min(28 / image.size[0], 28 / image.size[1])
+    new_size = (int(image.size[0] * aspect_ratio), int(image.size[1] * aspect_ratio))
+    image = image.resize(new_size, Image.LANCZOS)
+    return image
+
+def center_image(image):
+    background = Image.new('L', (28, 28), 255)
+    background.paste(image, (int((28 - image.size[0]) / 2), int((28 - image.size[1]) / 2)))
+    return background
+
+def invert_image(image):
+    inverted_image = ImageOps.invert(image)
+    return inverted_image
+
+def normalize_image(image_array):
+    image_array = image_array.astype(np.float32) / 255.0
+    return image_array
+
+def expand_dims(image_array):
+    image_array = np.expand_dims(image_array, axis=-1)
+    image_array = np.expand_dims(image_array, axis=0)
+    return image_array
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -5,7 +5,7 @@ def load_model(model_path):
     return keras_load_model(model_path)
 
 def make_prediction(model, preprocessed_image):
-    predictions = model.predict(preprocessed_image)
-    predicted_class = np.argmax(predictions, axis=1)[0]
-    confidence = np.max(predictions)
-    return predicted_class, confidence
+    prediction = model.predict(preprocessed_image)
+    predicted_number = np.argmax(prediction, axis=1)[0]
+    confidence_val = np.max(prediction)
+    return predicted_number, confidence_val
